152b9da49d46308c072d7be4cede9377a621942b
Stefan Deigmüller <stefan@layline.io>
2025-03-28 04:59:35 +0100

    Evaluation of configuration parameters added

--- ha-addon/run.sh
+++ ha-addon/run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/with-contenv bashio
 
-cp /config/uni-meter.conf /etc/uni-meter.conf
+if bashio::config.has_value 'custom_config' && [ -f "/config/$(bashio::config 'custom_config')" ]; then
+    bashio::log.info "using custom configuration file: $(bashio::config 'custom_config')"
+    cp "/config/$(bashio::config 'custom_config')" "/etc/uni-meter.conf"
+fi    
 
 /opt/uni-meter/bin/uni-meter.sh
