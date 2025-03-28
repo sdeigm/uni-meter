cb22cc7b6dbcb979f62b8b1b3ba0705f5f5d0969
Stefan Deigmüller <stefan@layline.io>
2025-03-28 09:14:19 +0100

    Echo output added

--- ha_addon/run.sh
+++ ha_addon/run.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/with-contenv bashio
 
+echo "Starting up uni-meter ..."
+
 if bashio::config.has_value 'custom_config' && [ -f "/config/$(bashio::config 'custom_config')" ]; then
     bashio::log.info "using custom configuration file: $(bashio::config 'custom_config')"
     cp "/config/$(bashio::config 'custom_config')" "/etc/uni-meter.conf"
