27d1b29306d5e18aced03aa479acb00b1bc9657c
Stefan Deigmüller <stefan@layline.io>
2025-04-15 09:30:48 +0200

    HTTP based mDNS service lookup added.

--- src/main/java/com/deigmueller/uni_meter/mdns/MDnsHomeAssistant.java
+++ src/main/java/com/deigmueller/uni_meter/mdns/MDnsHomeAssistant.java
@@ -1,10 +1,14 @@
 package com.deigmueller.uni_meter.mdns;
 
 import com.deigmueller.uni_meter.common.utils.NetUtils;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.TypeFactory;
 import com.typesafe.config.Config;
 import org.apache.pekko.actor.typed.Behavior;
+import org.apache.pekko.actor.typed.PostStop;
 import org.apache.pekko.actor.typed.javadsl.ActorContext;
 import org.apache.pekko.actor.typed.javadsl.Behaviors;
 import org.apache.pekko.actor.typed.javadsl.ReceiveBuilder;
@@ -16,7 +20,9 @@ import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.Duration;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
@@ -25,15 +31,24 @@ public class MDnsHomeAssistant extends MDnsKind {
   public static final String TYPE = "homeassistant";
   private final static Logger LOGGER = LoggerFactory.getLogger("uni-meter.mdns.ha");
   private static final ObjectMapper MAPPER = new ObjectMapper();
+  private static final String UNI_METER_MDNS_REGISTER = "uni_meter_mdns_register";
+  private static final String UNI_METER_MDNS_UNREGISTER = "uni_meter_mdns_unregister";
   
   // Instance members
   private final Http http = Http.get(getContext().getSystem());
   private final String url = getConfig().getString("url");
   private final HttpCredentials credentials = HttpCredentials.createOAuth2BearerToken(getConfig().getString("access-token"));
-  private final Materializer materializer = Materializer.createMaterializer(getContext());
+  private final Materializer materializer = Materializer.createMaterializer(getContext().getSystem());
   private final Set<RegisterService> pendingRegistrations = new HashSet<>();
+  private final Set<RegisterService> registeredServices = new HashSet<>();
+  private final String publicIpAddress = NetUtils.detectPrimaryIpAddress();
+  private boolean serviceDetectionErrorShown = false;
+  private boolean serviceRegistrationErrorShown = false;
   private boolean registrationRunning = false;
-  private boolean pyscriptAddonAvailable = false;
+  private boolean uniMeterMdnsRegisterAvailable = false;
+  private boolean uniMeterMdnsUnregisterAvailable = false;
+  private Duration serviceDetectionBackoff = Duration.ofSeconds(15);
+  private Duration retryRegistrationBackoff = Duration.ofSeconds(15);
 
   public static Behavior<Command> create(@NotNull Config config) {
     return Behaviors.setup(context -> new MDnsHomeAssistant(context, config));
@@ -46,16 +61,29 @@ public class MDnsHomeAssistant extends MDnsKind {
     LOGGER.debug("using url {}", config.getString("url"));
     LOGGER.debug("using access token {}", config.getString("access-token"));
     
-    detectPyscriptService();
+    startServiceDetection();
   }
 
   @Override
   public ReceiveBuilder<Command> newReceiveBuilder() {
     return super.newReceiveBuilder()
+          .onSignal(PostStop.class, this::onPostStop)
           .onMessage(ServiceDetectionSucceeded.class, this::onServiceDetectionSucceeded)
           .onMessage(ServiceDetectionFailed.class, this::onServiceDetectionFailed)
+          .onMessage(StartNextServiceDetection.class, this::onStartNextServiceDetection)
           .onMessage(ServiceRegistrationSucceeded.class, this::onServiceRegistrationSucceeded)
-          .onMessage(ServiceRegistrationFailed.class, this::onServiceRegistrationFailed);
+          .onMessage(ServiceRegistrationFailed.class, this::onServiceRegistrationFailed)
+          .onMessage(StartRetryRegistration.class, this::onStartRetryRegistration)
+          .onMessage(RegisterService.class, this::onRegisterService);
+  }
+  
+  private Behavior<Command> onPostStop(PostStop postStop) {
+    if (canUnregisterService()) {
+      for (RegisterService service : registeredServices) {
+        unregister(service.type(), service.name(), publicIpAddress, service.port(), service.properties());
+      }
+    }
+    return Behaviors.same();
   }
 
   /**
@@ -65,7 +93,45 @@ public class MDnsHomeAssistant extends MDnsKind {
    */
   private Behavior<Command> onServiceDetectionSucceeded(ServiceDetectionSucceeded message) {
     LOGGER.trace("MDnsHomeAssistant.onServiceDetectionSucceeded");
-    
+
+    try {
+      if (message.response.status() == StatusCodes.OK) {
+        LOGGER.debug("list services returned: {}", message.entity.getData().utf8String());
+        
+        List<Domain> domains = MAPPER.readValue(message.entity.getData().toArray(), CollectionsTypeFactory.listOfDomain());
+        for (Domain domain : domains) {
+          if (domain.domain().equals("pyscript")) {
+            uniMeterMdnsRegisterAvailable = domain.services().containsKey(UNI_METER_MDNS_REGISTER);
+            uniMeterMdnsUnregisterAvailable = domain.services().containsKey(UNI_METER_MDNS_UNREGISTER);
+            
+            break;
+          }
+        }
+
+        if (canRegisterNextService()) {
+          registerNextService();
+        }
+      } else {
+        if (! serviceDetectionErrorShown) {
+          LOGGER.error("failed to lookup available services, http request returned with status {}", message.response.status());
+          serviceDetectionErrorShown = true;
+        } else {
+          LOGGER.debug("failed to lookup available services, http request returned with status {}", message.response.status());
+        }
+      }
+    } catch (Exception e) {
+      if (! serviceDetectionErrorShown) {
+        LOGGER.error("service detection failed: {}", e.getMessage());
+        serviceDetectionErrorShown = true;
+      } else {
+        LOGGER.debug("service detection failed: {}", e.getMessage());
+      }
+    }
+
+    if (!uniMeterMdnsRegisterAvailable) {
+      startServiceDetectionTimer();
+    }
+
     return Behaviors.same();
   }
 
@@ -76,6 +142,22 @@ public class MDnsHomeAssistant extends MDnsKind {
    */
   private Behavior<Command> onServiceDetectionFailed(ServiceDetectionFailed message) {
     LOGGER.trace("MDnsHomeAssistant.onServiceDetectionFailed");
+
+    if (! serviceDetectionErrorShown) {
+      LOGGER.error("failed to lookup available services: {}", message.throwable().getMessage());
+      serviceDetectionErrorShown = true;
+    } else {
+      LOGGER.debug("failed to lookup available services: {}", message.throwable().getMessage());
+    }
+
+    startServiceDetectionTimer();
+    
+    return Behaviors.same();
+  }
+  
+  private Behavior<Command> onStartNextServiceDetection(StartNextServiceDetection message) {
+    LOGGER.trace("MDnsHomeAssistant.onStartNextServiceDetection");
+    startServiceDetection();
     return Behaviors.same();
   }
 
@@ -91,9 +173,23 @@ public class MDnsHomeAssistant extends MDnsKind {
     
     if (message.response.status() == StatusCodes.OK) {
       LOGGER.info("successfully registered mdns service {}", message.registerService.name());
+      serviceRegistrationErrorShown = false;
       pendingRegistrations.remove(message.registerService);
+      registeredServices.add(message.registerService);
+      
+      if (canRegisterNextService()) {
+        registerNextService();
+      }
     } else {
-        
+      if (! serviceRegistrationErrorShown) {
+        LOGGER.error("failed to register mdns service {}: http request returned with status {}", 
+              message.registerService.name(), message.response.status());
+        serviceRegistrationErrorShown = true;
+      } else {
+        LOGGER.debug("failed to register mdns service {}: http request returned with status {}",
+              message.registerService.name(), message.response.status());
+      }
+      startRetryRegistrationTimer();
     }
     
     return Behaviors.same();
@@ -103,8 +199,27 @@ public class MDnsHomeAssistant extends MDnsKind {
     LOGGER.trace("MDnsHomeAssistant.onServiceRegistrationFailed()");
     
     registrationRunning = false;
+
+    if (! serviceRegistrationErrorShown) {
+      LOGGER.error("mdns registration of service {} failed: {}", message.registerService.name(), 
+            message.throwable().getMessage());
+      serviceRegistrationErrorShown = true;
+    } else {
+      LOGGER.debug("mdns registration of service {} failed: {}", message.registerService.name(),
+            message.throwable().getMessage());
+    }
     
     return Behaviors.same();
+  }
+  
+  protected Behavior<Command> onStartRetryRegistration(StartRetryRegistration message) {
+    LOGGER.trace("MDnsHomeAssistant.onStartRetryRegistration()");
+    
+    if (canRegisterNextService()) { 
+      registerNextService();
+    }
+    
+    return Behaviors.same();   
   }
 
   @Override
@@ -113,7 +228,7 @@ public class MDnsHomeAssistant extends MDnsKind {
     
     pendingRegistrations.add(registerService);
     
-    if (pyscriptAddonAvailable && !registrationRunning) {
+    if (uniMeterMdnsRegisterAvailable && !registrationRunning) {
       registerNextService();
     }
     
@@ -123,7 +238,7 @@ public class MDnsHomeAssistant extends MDnsKind {
   /**
    * Try to detect the pyscript service
    */
-  private void detectPyscriptService() {
+  private void startServiceDetection() {
     LOGGER.debug("MDnsHomeAssistant.detectPyscriptService()");
     
     String path = url + "/api/services";
@@ -153,7 +268,6 @@ public class MDnsHomeAssistant extends MDnsKind {
               }
             }
           });
-    
   }
   
   private void registerNextService() {
@@ -172,8 +286,6 @@ public class MDnsHomeAssistant extends MDnsKind {
    */
   private void registerService(@NotNull RegisterService registerService) {
     String path = url + "/api/services/pyscript/uni_meter_mdns_register";
-    
-    String publicIpAddress = NetUtils.detectPrimaryIpAddress();
     
     String data;
     try {
@@ -197,36 +309,24 @@ public class MDnsHomeAssistant extends MDnsKind {
           .whenComplete((response, throwable) -> {
             if (throwable != null) {
               LOGGER.error("failed to register home assistant service: {}", throwable.getMessage());
-              getContext().getSelf().tell(new ServiceRegistrationFailed(throwable));
+              getContext().getSelf().tell(new ServiceRegistrationFailed(registerService, throwable));
             } else {
               try {
                 response.entity()
                       .toStrict(5000, materializer)
                       .whenComplete((strictEntity, toStrictFailure) -> {
                         if (toStrictFailure != null) {
-                          getContext().getSelf().tell(new ServiceRegistrationFailed(toStrictFailure));
+                          getContext().getSelf().tell(new ServiceRegistrationFailed(registerService, toStrictFailure));
                         } else {
                           getContext().getSelf().tell(new ServiceRegistrationSucceeded(registerService, response, strictEntity));
                         }
                       });
               } catch (Exception e) {
                 // Failed to get a strict entity
-                getContext().getSelf().tell(new ServiceRegistrationFailed(e));
+                getContext().getSelf().tell(new ServiceRegistrationFailed(registerService, e));
               }
             }
           });
-  }
-
-  @Override
-  public void unregister(@NotNull MDnsHandle handle) {
-    if (handle instanceof HaMDnsHandle haMDnsHandle) {
-      unregister(
-            haMDnsHandle.type(), 
-            haMDnsHandle.name(), 
-            haMDnsHandle.ip(), 
-            haMDnsHandle.port(), 
-            haMDnsHandle.properties());
-    }
   }
 
   public void unregister(@NotNull String type,
@@ -234,7 +334,7 @@ public class MDnsHomeAssistant extends MDnsKind {
                          @NotNull String ip,
                          int port,
                          @NotNull Map<String,String> properties) {
-    String path = url + "/api/services/pyscript/uni_meter_mdns_unregister";
+    String path = url + "/api/services/pyscript/" ;
 
     HttpRequest postRequest = HttpRequest.POST(path)
           .addCredentials(credentials)
@@ -253,6 +353,34 @@ public class MDnsHomeAssistant extends MDnsKind {
               }
             }
           });
+  }
+  
+  private boolean canRegisterNextService() {
+    return uniMeterMdnsRegisterAvailable && !pendingRegistrations.isEmpty() && !registrationRunning;
+  }
+  
+  private boolean canUnregisterService() {
+    return uniMeterMdnsUnregisterAvailable;
+  }
+  
+  private void startServiceDetectionTimer() {
+    getContext().getSystem().scheduler().scheduleOnce(serviceDetectionBackoff,
+          () -> getContext().getSelf().tell(StartNextServiceDetection.INSTANCE),
+          getContext().getExecutionContext());
+    serviceDetectionBackoff = serviceDetectionBackoff.plus(Duration.ofSeconds(15));
+    if (serviceDetectionBackoff.getSeconds() > 300) {
+      serviceDetectionBackoff = Duration.ofSeconds(300);
+    }
+  }
+  
+  private void startRetryRegistrationTimer() {
+    getContext().getSystem().scheduler().scheduleOnce(retryRegistrationBackoff,
+          () -> getContext().getSelf().tell(StartRetryRegistration.INSTANCE),
+          getContext().getExecutionContext());
+    retryRegistrationBackoff = retryRegistrationBackoff.plus(Duration.ofSeconds(15));
+    if (retryRegistrationBackoff.getSeconds() > 60) {
+      retryRegistrationBackoff = Duration.ofSeconds(60);
+    }
   }
   
   private @NotNull String getPayload(String type,
@@ -277,6 +405,7 @@ public class MDnsHomeAssistant extends MDnsKind {
   ) implements Command {}
 
   public record ServiceRegistrationFailed(
+        @NotNull RegisterService registerService,
         @NotNull Throwable throwable
   ) implements Command {}
   
@@ -286,11 +415,30 @@ public class MDnsHomeAssistant extends MDnsKind {
         @NotNull HttpEntity.Strict entity
   ) implements Command {}
   
+  public enum StartNextServiceDetection implements Command {
+    INSTANCE
+  }
+
+  public enum StartRetryRegistration implements Command {
+    INSTANCE
+  }
+  
   private record Payload(
-        String type, 
-        String name,
-        String ip,
-        int port,
-        Map<String,String> properties
+        @JsonProperty("type") String type,
+        @JsonProperty("name") String name,
+        @JsonProperty("ip") String ip,
+        @JsonProperty("port") int port,
+        @JsonProperty("properties") Map<String,String> properties
   ) {}
+  
+  private record Domain(
+        @JsonProperty("domain") String domain,
+        @JsonProperty("services") Map<String,Object> services
+  ) {}
+
+  private static class CollectionsTypeFactory {
+    static JavaType listOfDomain() {
+      return TypeFactory.defaultInstance().constructCollectionType(List.class, Domain.class);
+    }
+  }
 }
--- src/main/java/com/deigmueller/uni_meter/mdns/MDnsNone.java
+++ src/main/java/com/deigmueller/uni_meter/mdns/MDnsNone.java
@@ -1,21 +1,30 @@
 package com.deigmueller.uni_meter.mdns;
 
+import com.typesafe.config.Config;
+import org.apache.pekko.actor.typed.Behavior;
+import org.apache.pekko.actor.typed.javadsl.ActorContext;
+import org.apache.pekko.actor.typed.javadsl.Behaviors;
 import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.Map;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionStage;
+public class MDnsNone extends MDnsKind {
+  // Class members
+  public static final String TYPE = "none";
+  private final static Logger LOGGER = LoggerFactory.getLogger("uni-meter.mdns.none");
 
-public class MDnsNone implements MDnsKind {
-  @Override
-  public CompletionStage<MDnsHandle> register(@NotNull String type, 
-                                              @NotNull String name, 
-                                              int port, 
-                                              @NotNull Map<String, String> properties) {
-    return CompletableFuture.completedFuture(null);
+  public static Behavior<Command> create(@NotNull Config config) {
+    return Behaviors.setup(context -> new MDnsNone(context, config));
+  }
+
+  protected MDnsNone(@NotNull ActorContext<Command> context,
+                      @NotNull Config config) {
+    super(context, config);
   }
 
   @Override
-  public void unregister(@NotNull MDnsHandle handle) {
+  protected Behavior<Command> onRegisterService(@NotNull RegisterService registerService) {
+    LOGGER.trace("MDnsNone.onRegisterService()");
+    return Behaviors.same();
   }
 }
