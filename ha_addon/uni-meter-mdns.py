e0983f70a0c9fd41f58d071b3b9acbea8b4aeae6
Stefan Deigmüller <stefan@layline.io>
2026-02-07 17:43:18 +0100

    Version 1.2.0 (HA addon)

--- ha_addon/uni-meter-mdns.py
+++ ha_addon/uni-meter-mdns.py
@@ -5,26 +5,28 @@ def ip_to_bytes(ip):
     return bytes(map(int, ip.split('.')))
 
 @service
-def uni_meter_mdns_register(type, name, ip, port, properties):
+def uni_meter_mdns_register(type, name, ip, port, properties, server):
     aiozc = zeroconf.async_get_async_instance(hass)
     info = AsyncServiceInfo(
         type_=type + "._tcp.local.",
         name=name + "." + type + "._tcp.local.",
         addresses=[ip_to_bytes(ip)],
         port=port,
-        properties=properties
+        properties=properties,
+        server=server
     )
     aiozc.async_register_service(info)
 
 @service
-def uni_meter_mdns_unregister(type, name, ip, port, properties):
+def uni_meter_mdns_unregister(type, name, ip, port, properties, server):
     aiozc = zeroconf.async_get_async_instance(hass)
     info = AsyncServiceInfo(
         type_=type + "._tcp.local.",
         name=name + "." + type + "._tcp.local.",
         addresses=[ip_to_bytes(ip)],
         port=port,
-        properties=properties
+        properties=properties,
+        server=server
     )
     aiozc.async_unregister_service(info)
 
