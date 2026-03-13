c97c35d908f2b027d03cebe4a0946fa4a72adc7b
Stefan Deigmüller <stefan@layline.io>
2026-03-13 04:20:35 +0100

    Automatic obis channel detection also in binary mode

--- src/main/java/com/deigmueller/uni_meter/input/device/tibber/pulse/Pulse.java
+++ src/main/java/com/deigmueller/uni_meter/input/device/tibber/pulse/Pulse.java
@@ -29,6 +29,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -36,6 +37,7 @@ import java.util.regex.Pattern;
 public class Pulse extends HttpInputDevice {
   // Class members
   public static final String TYPE = "TibberPulse";
+  
   static final Pattern POWER_1_7_PATTERN = Pattern.compile("^1-0:1\\.7\\.0\\*255\\((-?[\\d.]+)\\*W\\)$");
   static final Pattern POWER_2_7_PATTERN = Pattern.compile("^1-0:2\\.7\\.0\\*255\\((-?[\\d.]+)\\*W\\)$");
   static final Pattern POWER_16_7_PATTERN = Pattern.compile("^1-0:16\\.7\\.0\\*255\\((-?[\\d.]+)\\*W\\)$");
@@ -50,6 +52,21 @@ public class Pulse extends HttpInputDevice {
   static final Pattern POWER_76_7_PATTERN = Pattern.compile("^1-0:76\\.7\\.0\\*255\\((-?[\\d.]+)\\*W\\)$");
   static final Pattern ENERGY_IMPORT_PATTERN = Pattern.compile("^1-0:1\\.8\\.0\\*255\\((-?[\\d.]+)\\*kWh\\)$");
   static final Pattern ENERGY_EXPORT_PATTERN = Pattern.compile("^1-0:2\\.8\\.0\\*255\\((-?[\\d.]+)\\*kWh\\)$");
+  
+  static final byte[] POWER_1_7_BYTES = new byte[] {0x01, 0x00, 0x01, 0x07, 0x00, (byte) 0xFF };
+  static final byte[] POWER_2_7_BYTES = new byte[] {0x01, 0x00, 0x02, 0x07, 0x00, (byte) 0xFF };
+  static final byte[] POWER_16_7_BYTES = new byte[] {0x01, 0x00, 0x10, 0x07, 0x00, (byte) 0xFF };
+  static final byte[] POWER_21_7_BYTES = new byte[] {0x01, 0x00, 0x15, 0x07, 0x00, (byte) 0xFF };
+  static final byte[] POWER_22_7_BYTES = new byte[] {0x01, 0x00, 0x16, 0x07, 0x00, (byte) 0xFF };
+  static final byte[] POWER_41_7_BYTES = new byte[] {0x01, 0x00, 0x29, 0x07, 0x00, (byte) 0xFF };
+  static final byte[] POWER_42_7_BYTES = new byte[] {0x01, 0x00, 0x2A, 0x07, 0x00, (byte) 0xFF };
+  static final byte[] POWER_61_7_BYTES = new byte[] {0x01, 0x00, 0x3D, 0x07, 0x00, (byte) 0xFF };
+  static final byte[] POWER_62_7_BYTES = new byte[] {0x01, 0x00, 0x3E, 0x07, 0x00, (byte) 0xFF };
+  static final byte[] POWER_36_7_BYTES = new byte[] {0x01, 0x00, 0x24, 0x07, 0x00, (byte) 0xFF };
+  static final byte[] POWER_56_7_BYTES = new byte[] {0x01, 0x00, 0x38, 0x07, 0x00, (byte) 0xFF };
+  static final byte[] POWER_76_7_BYTES = new byte[] {0x01, 0x00, 0x4C, 0x07, 0x00, (byte) 0xFF };
+  static final byte[] ENERGY_IMPORT_BYTES = new byte[] {0x01, 0x00, 0x01, 0x08, 0x00, (byte) 0xFF };
+  static final byte[] ENERGY_EXPORT_BYTES = new byte[] {0x01, 0x00, 0x02, 0x08, 0x00, (byte) 0xFF };
 
   // Instance members
   private final PhaseMode powerPhaseMode = getPhaseMode("power-phase-mode");
@@ -103,28 +120,6 @@ public class Pulse extends HttpInputDevice {
       }
     }
     return null;
-  }
-
-  static double findEnergyExportEntry(SmlGetListRes response) {
-    for (SmlListEntry entry : response.getValList().getValListEntry()) {
-      if (entry.getObjName().toHexString().equals("01 00 02 08 00 FF")) {
-        int energyScaler = entry.getScaler().getIntVal();
-        long energyValue = Long.parseLong(entry.getValue().toString());
-        return energyValue * Math.pow(10, energyScaler);
-      }
-    }
-    return 0.0;
-  }
-
-  static double findPowerEntry(SmlGetListRes response) {
-    for (SmlListEntry entry : response.getValList().getValListEntry()) {
-      if (entry.getObjName().toHexString().equals("01 00 10 07 00 FF")) {
-        int powerScaler = entry.getScaler().getIntVal();
-        long powerValue = Long.parseLong(entry.getValue().toString());
-        return powerValue * Math.pow(10, powerScaler);
-      }
-    }
-    return 0.0;
   }
 
   /**
@@ -251,17 +246,21 @@ public class Pulse extends HttpInputDevice {
 
     SmlGetListRes listResponse = findListResponse(messages);
     if (listResponse != null) {
-      double energyImport = findEnergyImportEntry(listResponse);
-      double energyExport = findEnergyExportEntry(listResponse);
-      double power = findPowerEntry(listResponse);
+      
+      checkPower(listResponse);
+      
+      Double energyImport = findEntry(ENERGY_IMPORT_BYTES, listResponse);
+      Double energyExport = findEntry(ENERGY_EXPORT_BYTES, listResponse);
 
-      logger.debug("Energy Import (Wh): {}", energyImport);
-      logger.debug("Energy Export (Wh): {}", energyExport);
-      logger.debug("Power (W): {}", power);
-
-      notifyPowerData(powerPhaseMode, powerPhase, power);
-
-      notifyEnergyData(energyPhaseMode, energyPhase, energyImport, energyExport);
+      if (energyImport != null || energyExport != null) {
+        logger.debug("Energy Import found (Wh): {}", energyImport != null ? energyImport : 0.0);
+        logger.debug("Energy Export found (Wh): {}", energyExport != null ? energyExport : 0.0);
+        notifyEnergyData(
+              energyPhaseMode,
+              energyPhase,
+              energyImport != null ? energyImport : 0.0,
+              energyExport != null ? energyExport : 0.0);
+      }
     }
   }
 
@@ -287,7 +286,22 @@ public class Pulse extends HttpInputDevice {
             energyExport != null ? energyExport : 0.0);
     }
   }
-  
+
+  private void checkPower(@NotNull SmlGetListRes response) {
+    if (checkFor21_7to61_7(response)) {
+      return;
+    }
+    if (checkFor36_7to76_7(response)) {
+      return;
+    }
+
+    if (checkFor16_7(response)) {
+      return;
+    }
+
+    checkFor1_7(response);
+  }
+
   private void checkPower(@NotNull String[] lines) {
     if (checkFor21_7to61_7(lines)) {
       return;
@@ -301,6 +315,26 @@ public class Pulse extends HttpInputDevice {
     }
 
     checkFor1_7(lines);
+  }
+
+  private boolean checkFor21_7to61_7(@NotNull SmlGetListRes response) {
+    Double impPowerL1 = findEntry(POWER_21_7_BYTES, response);
+    Double expPowerL1 = findEntry(POWER_22_7_BYTES, response);
+    Double impPowerL2 = findEntry(POWER_41_7_BYTES, response);
+    Double expPowerL2 = findEntry(POWER_42_7_BYTES, response);
+    Double impPowerL3 = findEntry(POWER_61_7_BYTES, response);
+    Double expPowerL3 = findEntry(POWER_62_7_BYTES, response);
+    if (impPowerL1 != null || impPowerL2 != null || impPowerL3 != null || expPowerL1 != null || expPowerL2 != null
+          || expPowerL3 != null) {
+      double l1 = (impPowerL1 != null ? impPowerL1 : 0.0) - (expPowerL1 != null ? expPowerL1 : 0.0);
+      double l2 = (impPowerL2 != null ? impPowerL2 : 0.0) - (expPowerL2 != null ? expPowerL2 : 0.0);
+      double l3 = (impPowerL3 != null ? impPowerL3 : 0.0) - (expPowerL3 != null ? expPowerL3 : 0.0);
+
+      notifyPowerData(l1, l2, l3);
+      return true;
+    }
+    
+    return false;
   }
 
   private boolean checkFor21_7to61_7(@NotNull String[] lines) {
@@ -322,6 +356,23 @@ public class Pulse extends HttpInputDevice {
     return false;
   }
   
+  private boolean checkFor36_7to76_7(@NotNull SmlGetListRes response) {
+    Double powerL1 = findEntry(POWER_36_7_BYTES, response);
+    Double powerL2 = findEntry(POWER_56_7_BYTES, response);
+    Double powerL3 = findEntry(POWER_76_7_BYTES, response);
+    if (powerL1 != null || powerL2 != null || powerL3 != null) {
+      double l1 = powerL1 != null ? powerL1 : 0.0;
+      double l2 = powerL2 != null ? powerL2 : 0.0;
+      double l3 = powerL3 != null ? powerL3 : 0.0;
+      
+      notifyPowerData(l1, l2, l3);
+      
+      return true;
+    }
+    
+    return false;
+  }
+  
   private boolean checkFor36_7to76_7(@NotNull String[] lines) {
     Double powerL1 = findEntry(POWER_36_7_PATTERN, lines);
     Double powerL2 = findEntry(POWER_56_7_PATTERN, lines);
@@ -332,6 +383,19 @@ public class Pulse extends HttpInputDevice {
       double l3 = powerL3 != null ? powerL3 : 0.0;
 
       notifyPowerData(l1, l2, l3);
+      
+      return true;
+    }
+    
+    return false;
+  }
+  
+  private boolean checkFor16_7(@NotNull SmlGetListRes response) {
+    Double power = findEntry(POWER_16_7_BYTES, response);
+    if (power != null) {
+      logger.debug("Power 16.7.0 found (W): {}", power);
+      
+      notifyPowerData(powerPhaseMode, powerPhase, power);
       
       return true;
     }
@@ -351,8 +415,19 @@ public class Pulse extends HttpInputDevice {
 
     return false;
   }
+  
+  private void checkFor1_7(@NotNull SmlGetListRes response) {
+    Double impPower = findEntry(POWER_1_7_BYTES, response);
+    Double expPower = findEntry(POWER_2_7_BYTES, response);
+    if (impPower != null || expPower != null) {
+      double power = (impPower != null ? impPower : 0.0) - (expPower != null ? expPower : 0.0);
+      
+      logger.debug("Power found (W): {}", power);
+      
+      notifyPowerData(powerPhaseMode, powerPhase, power);
+    }
+  }
 
-  @SuppressWarnings("unused")
   private void checkFor1_7(@NotNull String[] lines) {
     Double impPower = findEntry(POWER_1_7_PATTERN, lines);
     Double expPower = findEntry(POWER_2_7_PATTERN, lines);
@@ -365,7 +440,7 @@ public class Pulse extends HttpInputDevice {
     }
   }
 
-  private SmlGetListRes findListResponse(List<SmlMessage> messages) {
+  private @Nullable SmlGetListRes findListResponse(List<SmlMessage> messages) {
     for (SmlMessage message : messages) {
       logger.debug("Found tag {}", message.getMessageBody().getTag());
       if (message.getMessageBody().getTag() == EMessageBody.GET_LIST_RESPONSE) {
@@ -374,19 +449,19 @@ public class Pulse extends HttpInputDevice {
     }
     return null;
   }
-
-  double findEnergyImportEntry(SmlGetListRes response) {
+  
+  private @Nullable Double findEntry(byte @NotNull[] name, @NotNull SmlGetListRes response) {
     for (SmlListEntry entry : response.getValList().getValListEntry()) {
-      logger.debug("Found OBIS code {}", entry.getObjName().toHexString());
-      if (entry.getObjName().toHexString().equals("01 00 01 08 00 FF")) {
+      if (Arrays.equals(entry.getObjName().getValue(), name)) {
         int energyScaler = entry.getScaler().getIntVal();
         long energyValue = Long.parseLong(entry.getValue().toString());
         return energyValue * Math.pow(10, energyScaler);
       }
     }
-    return 0.0;
+    
+    return null;
   }
-
+  
   private void executePulseStatusPolling() {
     logger.trace("Pulse.executePulseStatusPolling()");
 
@@ -435,16 +510,13 @@ public class Pulse extends HttpInputDevice {
 
   protected record PulseStatusRequestFailed(
         @NotNull Throwable throwable
-  ) implements Command {
-  }
+  ) implements Command {}
 
   protected record PulseStatusRequestSuccess(
         @NotNull HttpResponse response
-  ) implements Command {
-  }
+  ) implements Command {}
 
   protected record StrictEntity(
         @NotNull HttpEntity.Strict entity
-  ) implements Command {
-  }
+  ) implements Command {}
 }
